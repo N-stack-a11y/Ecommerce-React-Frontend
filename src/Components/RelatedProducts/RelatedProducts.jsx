562b732e1376031a7a9e637f8eeb448954cba758
mayurnandwalkar <mayur@owowtalents.comgit>
2024-12-05 22:06:44 +0530

    code updated

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
-import './RelatedProducts.css'
-import data_product from '../Assets/data'
-import Item from '../Item/Item'
+import React from 'react';
+import './RelatedProducts.css';
+import Item from '../Item/Item';
 
-const RelatedProducts = () => {
+// Function to shuffle an array
+const shuffleArray = (array) => {
+  return array.sort(() => Math.random() - 0.5);
+};
+
+const RelatedProducts = (props) => {
+  const shuffledProducts = shuffleArray(props?.all_product || []);
+
   return (
     <div className="relatedproducts">
-        <h1>RELATED PRODUCTS</h1>
-        <hr/>
-        <div className="relatedproducts-item">
-            {data_product.map((item,i)=>{
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
-            })}
-
-        </div>
+      <h1>RELATED PRODUCTS</h1>
+      <hr />
+      <div className="relatedproducts-item">
+        {shuffledProducts.slice(0, 4).map((item, i) => (
+          <Item
+            key={i}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default RelatedProducts
+export default RelatedProducts;
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -7,16 +7,16 @@ import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
 import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
 
 const Product = () => {
-  const {all_product}=useContext(ShopContext);
-  const {productId} = useParams();
-  const product = all_product.find((e)=> e.id === Number(productId));
-  console.log(product,"product+++++++++++++");
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
+  console.log(all_product,"test")
   return (
     <div>
-      <Bredcrums product={product}/>
-      <ProductDisplay product={product}/>
-      <DescriptionBox/>
-      <RelatedProducts/>
+      <Bredcrums product={product} />
+      <ProductDisplay product={product} />
+      <DescriptionBox />
+      <RelatedProducts all_product={all_product} />
     </div>
   )
 }
